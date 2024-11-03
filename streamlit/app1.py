3ba584b11cb9b98ed53b680d73bb3d2058830296
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-11-04 00:34:20 +0530

    comparison of different regions

--- streamlit/app1.py
+++ streamlit/app1.py
@@ -9,9 +9,22 @@ def distinct_region():
     tempset = set(templ)
     return list(tempset)
 
-# unique_regions = distinct_region()
-# print(unique_regions)
+unique_regions = distinct_region()
+print(unique_regions)
 
-tempdf = df.loc[df["Region"] == "Western Europe"]
-plt.plot(tempdf["Region"])
-plt.show()
+def western_europe_comparison():
+    tempdf = df.loc[df["Region"] == "Western Europe"]
+    plt.figure(figsize=(10, 6))
+    plt.bar(tempdf["Country"], height=tempdf["Freedom"])
+    plt.show()
+    # print(tempdf)
+
+def middleast_compare():
+    tempdf = df.loc[df["Region"] == "Middle East and Northern Africa"]
+    # print(tempdf)
+    plt.figure(figsize=(10, 6))
+    plt.bar(tempdf["Country"], height=tempdf["Freedom"])
+    plt.show()
+
+
+middleast_compare()
