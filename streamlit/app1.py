d530596e61db1d2aa9295c3a0f793345ab2084b1
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-11-04 00:18:03 +0530

    temporary dataframes

--- streamlit/app1.py
+++ streamlit/app1.py
@@ -4,4 +4,14 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("D:\\Bennett University\\Sem 3\\Projects\\DataScience\\trialone\\edac.csv")
 
+def distinct_region():
+    templ = df["Region"]
+    tempset = set(templ)
+    return list(tempset)
 
+# unique_regions = distinct_region()
+# print(unique_regions)
+
+tempdf = df.loc[df["Region"] == "Western Europe"]
+plt.plot(tempdf["Region"])
+plt.show()
