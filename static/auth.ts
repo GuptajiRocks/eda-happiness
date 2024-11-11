0546eafe3d30a018a8cf031da2ff4854915c50df
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-11-11 11:39:30 +0530

    auth.ts embed

---
+++ static/auth.ts
@@ -0,0 +1,11 @@
+const baz = () => console.log("baz");
+const foo = () => console.log("foo");
+const zoo = () => console.log("zoo");
+
+// const start = () => {
+//     console.log("start");
+//     setImmediate(baz)
+//     const promise((resolve, react)) {
+
+//     }
+// }
